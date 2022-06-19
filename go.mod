42ec023fd0a68f3078bb42e0dbee7309d531725e
Sherzod <qurbonovsherzod2002@gmail.com>
2022-06-19 11:40:47 +0500

    adding module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Qurb0n0vSh2oo2/bankep
+
+go 1.18
---
+++ pkg/bank/types/types.go
@@ -0,0 +1,29 @@
+package types
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type PAN string
+
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type Payment struct {
+	ID     int
+	Amount Money
+}
